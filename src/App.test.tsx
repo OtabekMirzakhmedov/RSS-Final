3b90a32e8921d8ebf9c7788e7cf2b33929af8873
User <User@DESKTOP-C51DDIB>
2024-05-02 13:14:28 +0500

    feat: example test

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { screen, render } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import App from './App.tsx';
+
+describe('App tests', () => {
+  it('should render the title', () => {
+    render(<App />);
+
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Vite + React');
+  });
+});
