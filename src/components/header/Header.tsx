7c2f3b8bdb8edec3fed5df1290193c201363f1b5
EVISION\Otabek.Mirzakhmedov <otabek.mirzakhmedov@wolterskluwer.com>
2024-05-20 11:33:21 +0500

    feat: links to pages

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,20 +3,39 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import { useNavigate } from 'react-router-dom';
 import './header.scss';
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+  const handleRegistrationClick = () => {
+    navigate('/create-account');
+  };
   return (
     <AppBar position='static' className='app-bar'>
       <Toolbar>
-        <Typography variant='h4' component='div' className='title'>
+        <Typography variant='h4' component='div' className='title' onClick={handleLogoClick}>
           HardBooks
         </Typography>
         <Box className='button-box'>
-          <Button variant='contained' color='success'>
+          <Button variant='contained' color='success' onClick={handleLoginClick}>
             Login
           </Button>
-          <Button variant='outlined' color='inherit' className='registration-button'>
+          <Button
+            variant='outlined'
+            color='inherit'
+            className='registration-button'
+            onClick={handleRegistrationClick}
+          >
             Registration
           </Button>
         </Box>
