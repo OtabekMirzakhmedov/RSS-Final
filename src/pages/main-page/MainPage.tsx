beb70b48e0ca332fc22f955b810cb261cb16422c
EVISION\Otabek.Mirzakhmedov <otabek.mirzakhmedov@wolterskluwer.com>
2024-05-20 11:35:35 +0500

    fix: remove unnecessary links from main page

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,16 +1,9 @@
-import { Link } from 'react-router-dom';
 import Header from '../../components/header/Header';
 
 function MainPage() {
   return (
     <>
       <Header />
-      <div>
-        <Link to='/login'>link to login</Link>
-      </div>
-      <div>
-        <Link to='/create-account'>link to Registration</Link>
-      </div>
       <h1>MainPage</h1>
     </>
   );
