92fa674e5a6e4051693be68e26c0cacec170b22f
Kirill <k187m18733@gmail.com>
2024-05-21 13:10:26 +0300

    feat: add burger menu and fix responsive on login page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,15 @@
-/* eslint-disable no-console */
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { getAccessToken } from '../../service/AuthenticationService';
 import './header.scss';
@@ -11,6 +17,7 @@ import './header.scss';
 function Header() {
   const navigate = useNavigate();
   const isLoggedin = localStorage.getItem('token') !== null;
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
 
   const handleLogoClick = () => {
     navigate('/');
@@ -33,7 +40,7 @@ function Header() {
         navigate('/');
       })
       .catch((error) => {
-        console.error('Error logging out:', error);
+        throw new Error(`Error logging out: ${error}`);
       });
   };
 
@@ -43,7 +50,7 @@ function Header() {
         <Typography variant='h4' component='div' className='title' onClick={handleLogoClick}>
           HardBooks
         </Typography>
-        <Box className='button-box'>
+        <Box className='button-box' sx={{ display: { xs: 'none', sm: 'flex' } }}>
           {isLoggedin ? (
             <Button variant='contained' color='error' onClick={handleLogoutClick}>
               Logout
@@ -65,12 +72,65 @@ function Header() {
                 disabled={location.pathname === '/create-account'}
                 onClick={handleRegistrationClick}
               >
-                Registration
+                Register
               </Button>
             </>
           )}
         </Box>
+        <IconButton
+          className='burger'
+          edge='end'
+          color='inherit'
+          aria-label='menu'
+          sx={{ display: { xs: 'flex', sm: 'none' } }}
+          onClick={() => setIsDrawerOpen(true)}
+        >
+          <MenuIcon />
+        </IconButton>
       </Toolbar>
+      <Drawer
+        anchor='right'
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        sx={{ display: { xs: 'flex', sm: 'none' } }}
+      >
+        <IconButton onClick={() => setIsDrawerOpen(false)}>
+          <ChevronRightIcon />
+        </IconButton>
+        <List>
+          {isLoggedin ? (
+            <ListItem>
+              <Button variant='contained' color='error' onClick={handleLogoutClick}>
+                Logout
+              </Button>
+            </ListItem>
+          ) : (
+            <>
+              <ListItem>
+                <Button
+                  variant='contained'
+                  color='success'
+                  disabled={location.pathname === '/login'}
+                  onClick={handleLoginClick}
+                >
+                  Login
+                </Button>
+              </ListItem>
+              <ListItem>
+                <Button
+                  variant='outlined'
+                  color='inherit'
+                  className='registration-button'
+                  disabled={location.pathname === '/create-account'}
+                  onClick={handleRegistrationClick}
+                >
+                  Register
+                </Button>
+              </ListItem>
+            </>
+          )}
+        </List>
+      </Drawer>
     </AppBar>
   );
 }
