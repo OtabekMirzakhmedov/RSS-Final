c9149d013e9d2a44db38243a413fd8effb6c4ce0
OtabekMirzakhmedov <otabek.mirzakhmedov@gmail.com>
2024-05-30 16:04:01 +0500

    feat: getting products from ecommercetools (#39)
    * feat: getting products from ecommercetools
    
    * fix: error with comment
    
    ---------
    
    Co-authored-by: ITRANSITION\o.mirzakhmedov <o.mirzakhmedov@itransition.com>

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,10 +1,61 @@
+import { useEffect, useState } from 'react';
 import Header from '../../components/header/Header';
+import { GetProducts } from '../../service/ProductService';
+
+interface MainPageProduct {
+  id: string;
+  title: string;
+  author?: string;
+  image?: string;
+  price: number;
+}
 
 function MainPage() {
+  const [products, setProducts] = useState<MainPageProduct[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const fetchedProducts = await GetProducts();
+        if (fetchedProducts) {
+          setProducts(fetchedProducts);
+        } else {
+          setError(new Error('Failed to fetch products'));
+        }
+      } catch (fetchError) {
+        setError(fetchError as Error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetchProducts();
+  }, []);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <>
       <Header />
       <h1>MainPage</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h2>{product.title}</h2>
+            <p>Author: {product.author}</p>
+            <p>Price: ${product.price.toFixed(2)}</p>
+            {product.image && <img src={product.image} alt={product.title} />}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
