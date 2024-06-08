82a5c2aa83376fec043cd857a049ab0b77d04159
Kirill <k187m18733@gmail.com>
2024-06-08 23:36:17 +0300

    feat: add adaptive for main text

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -15,23 +15,28 @@ function MainPage() {
         textAlign='center'
         px={5}
       >
-        <Typography variant='h1' gutterBottom>
+        <Typography
+          variant='h1'
+          fontWeight='500'
+          gutterBottom
+          sx={{ fontSize: { xs: '3rem', md: '5rem' } }}
+        >
           Welcome to Book Haven
         </Typography>
-        <Typography variant='h4' gutterBottom>
+        <Typography variant='h4' gutterBottom sx={{ fontSize: { xs: '1.2rem', md: '2rem' } }}>
           Your one-stop online shop for all your reading needs
         </Typography>
-        <Typography variant='h5' gutterBottom>
+        <Typography variant='h5' gutterBottom sx={{ fontSize: { xs: '1.2rem', md: '2rem' } }}>
           Browse our extensive catalog, find your next favorite book, and enjoy seamless shopping
           from the comfort of your home.
         </Typography>
-        <Typography variant='h5' gutterBottom>
+        <Typography variant='h5' gutterBottom sx={{ fontSize: { xs: '1.2rem', md: '2rem' } }}>
           Discover new releases, bestsellers, and timeless classics.
         </Typography>
-        <Typography variant='h5' gutterBottom>
+        <Typography variant='h5' gutterBottom sx={{ fontSize: { xs: '1.2rem', md: '2rem' } }}>
           Whether you are into fiction, non-fiction, or biographies, we have something for everyone.
         </Typography>
-        <Typography variant='h6' gutterBottom>
+        <Typography variant='h6' gutterBottom sx={{ fontSize: { xs: '1.2rem', md: '2rem' } }}>
           Start exploring now and immerse yourself in the world of books!
         </Typography>
       </Box>
