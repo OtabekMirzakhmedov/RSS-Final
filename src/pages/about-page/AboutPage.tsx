624198b6b4eee15e2e001fe0bcb2ddff7e4e6d06
Kirill <k187m18733@gmail.com>
2024-06-08 23:36:59 +0300

    feat: implement about page without content

---
+++ src/pages/about-page/AboutPage.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material';
+import Header from '../../components/header/Header';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <div>text</div>
+      </Container>
+    </>
+  );
+}
